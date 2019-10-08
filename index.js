943c29d7932632a3fa3865318f7c0a25d35958c1
Irina Kagan <irinagamb@gmail.com>
2019-10-08 01:44:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
 document.getElementById('main').remove();
-let newHeader = document.createElement('h1');
-newHeader.setAttribute('id', 'victory');
-newHeader.innerHTML = 'Chrissy is the champion';
-document.appendChild(newHeader);
