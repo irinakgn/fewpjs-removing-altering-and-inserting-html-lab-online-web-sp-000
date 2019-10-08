6a367a7bef7cc61901f20939f7f23a3397c88479
Irina Kagan <irinagamb@gmail.com>
2019-10-08 01:32:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 main.remove()
 let newHeader = document.getElementById("div#header");
-newHeader.innerHTML = '<h1 id="victory">YOUR-NAME is the champion</h1>'';
+newHeader.innerHTML = '<h1 id="victory">YOUR-NAME is the champion</h1>';
