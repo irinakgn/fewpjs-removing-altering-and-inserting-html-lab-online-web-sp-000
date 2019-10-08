32d97052e02722b3db565e35cd80e051eeab6203
Irina Kagan <irinagamb@gmail.com>
2019-10-08 01:53:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 document.getElementById('main').remove();
 
 const newHeader = document.createElement('h1')
-newHeader.setAttribute('id', 'victory');
-
+newHeader.id = 'victory'
 newHeader.innerHTML = "YOUR-NAME is the champion"
 
 
